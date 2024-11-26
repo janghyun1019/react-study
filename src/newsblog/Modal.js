6b2fca63f30ebc02ec99ebf6ebba5e6b5247c74d
janghyun1019 <mon.p.a.hyun@gmail.com>
2024-11-26 16:08:21 +0900

    NewsBlog 반복 처리 전

---
+++ src/newsblog/Modal.js
@@ -0,0 +1,15 @@
+
+function Modal({ setModalVisible, title }) {
+
+    return (
+        <div className="modal">
+        <h3>{title}</h3>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+    )
+}
+
+export default Modal;
+
+
